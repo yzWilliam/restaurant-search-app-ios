8e1522ba7a8d7c311177ea580f4c291a2b1c98ed
Will-Zhiwei <zy1129@nyu.edu>
2021-01-26 08:04:49 -0800

    Initial Commit

---
+++ restaurantSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  restaurantSearchApp
+//
+//  Created by Zhiwei Yu on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
